cadfdef4e19712cef21d0ef85aaa51c4f5d89993
sujal maidamwar <technoidsujal@gmail.com>
2026-04-17 10:42:48 +0530

    Adding Devops

---
+++ Calc/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace calctest;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"Hello World");
+        Console.WriteLine(Add2Nos(10,20));
+        
+        
+    }
+
+    public static int Add2Nos(int v1, int v2)
+    {
+        return v1+v2;
+    }
+}
---
+++ calctest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace calctest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        int a = 10;
+        int b = 20;
+        int expected = 30;
+
+
+        int result = Program.Add2Nos(a,b);
+
+        Assert.Equal(expected,result);
+    }
+}
